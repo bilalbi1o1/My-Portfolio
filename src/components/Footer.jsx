b7f1e6c2a7fde4ee816ab72f32e3ddd88d7a3f38
Bilal Ahmad <bilalba4oo4@gmail.com>
2024-08-11 08:19:19 -0700

    UI adjustment for mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ import Socials from './Socials'
 
 const Footer = () => {
   return (
-    <div className='bg-slate-800 w-full h-16 flex items-center justify-between xl:mt-5'>
+    <div className='max-sm:fixed max-sm:bottom-0 mb-0 bg-slate-800 w-full h-16 flex items-center justify-between xl:mt-5'>
       <div>
-        <Socials containerStyles='flex pl-8 gap-6' iconStyles='h-9 w-9 border  
+        <Socials containerStyles='flex pl-2 sm:pl-8 gap-6' iconStyles='h-9 w-9 border  
                 border-accent rounded-full flex justify-center items-center text-accent 
                 text-base hover:bg-accent hover:text-primary hover:transition-all duration-500'/>
       </div>
